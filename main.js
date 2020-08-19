343b5e630f1bc738b28eb75ed81180b75b6a3914
wcourtney31415 <wcourtney31415@gmail.com>
2020-08-19 01:23:34 -0400

    Made the code accept a few parameters regarding username, password, and whom to send the email to, then generate an email body that lists the ipv4 address, ipv6 address, and time of generation.

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const publicIp = require('public-ip');
 
-var transporter = nodemailer.createTransport({
+(async () => {
+  const ipv4 = await publicIp.v4();
+  const ipv6 = await publicIp.v6();
+  const message = generateMessage(ipv4, ipv6);
+  const mailOptions = optionsWith(message);
+  sendEmail(mailOptions);
+})();
+
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.USER,
@@ -8,17 +17,44 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
-  from: '',
-  to: process.env.TO,
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+function optionsWith(text) {
+  const options = {
+    from: '',
+    to: process.env.TO,
+    subject: "IP Report",
+    text: text
+  };
+  return options;
 };
 
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+function sendEmail(mailOptions) {
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(info.response);
+    }
+  });
+}
+
+function now() {
+  const time = new Date();
+  const date = ("0" + time.getDate()).slice(-2);
+  const month = ("0" + (time.getMonth() + 1)).slice(-2);
+  const year = time.getFullYear();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const wholeDate = month + "-" + date + "-" + year;
+  const wholeTime = hours + ":" + minutes + ":" + seconds;
+  return (wholeDate + " " + wholeTime);
+}
+
+function generateMessage(ipv4, ipv6) {
+  const v4Text = `IPv4:\n${ipv4}`;
+  const v6Text = `IPv6:\n${ipv6}`;
+  const timeText = `As of:\n${now()}`;
+  const vSpace = '\n\n'
+  const message = v4Text + vSpace + v6Text + vSpace + timeText;
+  return message;
+}
